e30acbf573216596837bf49f8693f81357238c76
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-10-20 00:54:24 +0100

    sheet 2 Q1 solution finished

--- src/example_sheet_02/Sht02Q01/Q0201.lean
+++ src/example_sheet_02/Sht02Q01/Q0201.lean
@@ -1,25 +1,26 @@
 import algebra.group_power data.real.basic
 
-section M1F_Sheet02
+def countable_union_from_zero {α : Type} (X : nat → set α) :=
+{t : α | exists i, t ∈ X i}
 
-def countable_union_from_zero {α : Type} (X : nat → set α) := {t : α | exists i, t ∈ X i}
-def countable_union_from_one {α : Type} (X : nat → set α) := {t : α | exists i, 0 < i ∧ t ∈ X i}
-def countable_intersection_from_one {α : Type} (X : nat → set α) := {t : α | ∀ i, 0 < i → t ∈ X i}
+def countable_union_from_one {α : Type} (X : nat → set α) :=
+{t : α | exists i, 0 < i ∧ t ∈ X i}
+
+def countable_intersection_from_one {α : Type} (X : nat → set α) :=
+{t : α | ∀ i, 0 < i → t ∈ X i}
 
 def Q0201a_sets (n : ℕ) : set ℝ := {x | ↑n ≤ x ∧ x < (n+1)}
 
-theorem Q0201a : countable_union_from_zero Q0201a_sets = {x | 0 ≤ x} := sorry
+theorem Q0201a : countable_union_from_zero Q0201a_sets = ??? := sorry
 
 def Q0201b_sets (n : ℕ) : set ℝ := {x | 1/(↑n) ≤ x ∧ x ≤ 1}
 
-theorem Q0201b : countable_union_from_one Q0201b_sets = { x | 0 < x ∧ x ≤ 1} := sorry 
+theorem Q0201b : countable_union_from_one Q0201b_sets = ??? := sorry 
 
 def Q0201c_sets (n : ℕ) : set ℝ := {x | -↑n < x ∧ x < n}
 
-theorem Q0201c : countable_union_from_one Q0201c_sets = set.univ := sorry
+theorem Q0201c : countable_union_from_one Q0201c_sets = ??? := sorry
 
 def Q0201d_sets (n : ℕ) : set ℝ := {x | -↑n < x ∧ x < n}
 
-theorem Q0201d : countable_intersection_from_one Q0201d_sets = {x | -1 < x ∧ x < 1} := sorry
-
-end M1F_Sheet02
+theorem Q0201d : countable_intersection_from_one Q0201d_sets = ??? := sorry
