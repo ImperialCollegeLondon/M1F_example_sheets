8788d43e0fdc46e6f8fcfb13f6e0a06aeee60b4b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-10-20 11:33:31 +0100

    sheet 2 question 2 formalised

--- src/example_sheet_02/Sht02Q02/Q0202.lean
+++ src/example_sheet_02/Sht02Q02/Q0202.lean
@@ -1,5 +1,8 @@
 import data.real.basic
 
-def open_zero_one := { x : ℝ | 0<x ∧ x<1}
+def open_zero_one := {x : ℝ | 0 < x ∧ x < 1}
 
-theorem Q2 : forall x : ℝ, x ∈ open_zero_one → ∃ y : ℝ, y ∈ open_zero_one ∧ x<y := sorry
+-- open_zero_one has no maximal element
+theorem Q2 : ¬ (∃ m : ℝ, m ∈ open_zero_one ∧ 
+                  ∀ x : ℝ, x ∈ open_zero_one → x ≤ m) := sorry
+        
