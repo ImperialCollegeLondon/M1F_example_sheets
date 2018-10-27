642ff0b1f1490173ff7f86f368674e9d47862c09
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-10-27 18:11:41 +0100

    finished Q6

--- src/example_sheet_02/Sht02Q06/S0206.lean
+++ src/example_sheet_02/Sht02Q06/S0206.lean
@@ -11,7 +11,15 @@ begin
   rw [(sqrt_prop 15).2,add_mul_self_eq,(sqrt_prop 2).2,(sqrt_prop 6).2] at H2,
   have H3 : 15 ≤ 2 + (6 + 2 * sqrt 2 * sqrt 6),
     revert H2,norm_num,
-  rw sub_le
+  rw [←sub_le_iff_le_add',←sub_le_iff_le_add',mul_assoc,
+      ←div_le_iff' (show (0 : ℝ) < 2, by norm_num)] at H3,
+  have H4 := mul_self_le_mul_self (by norm_num) H3,
+  rw mul_assoc at H4,
+  rw mul_comm (sqrt 6) at H4,
+  rw mul_assoc at H4,
+  rw (sqrt_prop 6).2 at H4,
+  rw ←mul_assoc at H4,
+  rw (sqrt_prop 2).2 at H4,
+  revert H4,
+  norm_num
 end
-
-#check add_mul_self
