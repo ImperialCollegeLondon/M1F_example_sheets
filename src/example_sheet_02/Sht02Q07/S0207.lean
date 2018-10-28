4f548de01538e247e1d91829481f315ded56a9be
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-10-28 20:40:18 +0000

    finished Q7

--- src/example_sheet_02/Sht02Q07/S0207.lean
+++ src/example_sheet_02/Sht02Q07/S0207.lean
@@ -1,10 +1,9 @@
 import data.real.irrational
 import xenalib.irrational
 import tactic.norm_num
+import example_sheet_02.Sht02Q03.S0203
 
 open real
-
--- delete as appropriate
 
 theorem Q0207a_irrat : irrational (sqrt 2 + sqrt (3 / 2)) := 
 begin
@@ -17,12 +16,36 @@ begin
   rw [mul_assoc,←sqrt_mul] at H2,
   rw (show max (0 : ℝ) 2 = 2, by norm_num) at H2,
   rw (show max (0 : ℝ) (3 / 2) = 3 / 2, by norm_num) at H2,
-  rw (show (2 : ℝ) * (3 / 2) = 3, by norm_num) at H2,
-  sorry
+  rw (show (2 : ℝ) * (3 / 2) = 3, by norm_num) at H2,swap,norm_num,
+  have H3 : sqrt 3 = (q * q - 2 - 3 / 2) / 2,
+  rw ←H2,ring,
+  have H4 : (((q * q - 2 - 3 / 2) / 2) : ℝ) = (((q * q - 2 - 3 / 2) / 2) : ℚ),
+    simp,
+  have H5 : (sqrt 3) ^ 2 = 3,
+    rw [pow_two,(sqrt_prop 3).2],
+    norm_num, 
+  rw H3 at H5,
+  rw H4 at H5,
+  rw (show (3 : ℝ) = (3 : ℚ), by norm_num) at H5,
+  apply no_rational_squared_is_three, -- from sheet 2 question 3
+  existsi (q * q - 2 - 3 / 2) / 2,
+  rw ←@rat.cast_inj ℝ _ _,
+  rw ←H5,
+  rw pow_two,
+  rw pow_two,
+  simp,  
 end
 
-theorem Q0207b_irrat : irrational (1 + sqrt 2 + sqrt (3 / 2)) := sorry
-theorem Q0207b_rat : ∃ (q : ℚ), 1 + sqrt 2 + sqrt (3 / 2) = q := sorry
+
+theorem Q0207b_irrat : irrational (1 + sqrt 2 + sqrt (3 / 2)) := 
+begin
+  rintro ⟨q,Hq⟩,
+  apply Q0207a_irrat,
+  existsi (q-1),
+  rw rat.cast_sub,
+  rw ←Hq,
+  simp,
+end
 
 theorem Q0207c_rat : ∃ (q : ℚ), 2 * sqrt 18 - 3 * sqrt 8 = q := ⟨0,begin 
   rw [(show ((0 : ℚ) : ℝ) = 0, by simp),sub_eq_zero],
