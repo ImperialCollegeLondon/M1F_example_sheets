f85bcec4a5a5ef59699bb5ab4191ec0cf8cc709d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-14 17:36:41 +0000

    more sheet 3

--- src/example_sheet_03/Sht03Q03/S0303.lean
+++ src/example_sheet_03/Sht03Q03/S0303.lean
@@ -1,6 +1,7 @@
 import analysis.real
 import xenalib.real_nth_root
 import tactic.norm_num
+import tactic.explode
 
 -- preliminary lemma
 
@@ -60,9 +61,74 @@ begin
   exact pow_mono (by norm_num) dec_trivial (by norm_num),
 end
 
+-- end of Q3a
+
+-- Q3b
+
+-- nat.pow_lt_pow_of_lt_right :
+-- ∀ {x : ℕ}, x > 1 → ∀ {i j : ℕ}, i < j → x ^ i < x ^ j
+
+variable (x : ℝ) -- or [linear_ordered_field]
+theorem pow_lt_pow_of_lt {i j : ℕ} (Hx : x > 1) (Hij : i < j) : x^i < x^j :=
+begin
+  sorry
+end
+
+open nat
+
+theorem nat.pow_lt_pow_of_lt_right' {x : ℕ} (H : x > 1) {i j : ℕ} (h : i < j) : x^i < x^j :=
+begin
+  have xpos := lt_of_succ_lt H,
+  refine lt_of_lt_of_le _ (pow_le_pow_of_le_right xpos h),
+  rw [← mul_one (x^i), nat.pow_succ],
+  exact nat.mul_lt_mul_of_pos_left H (pos_pow_of_pos _ xpos)
+end
+
+/- Mario proof -/
+variables {α : Type*} [linear_ordered_semiring α]
+theorem pow_lt_pow {a : α} {n m : ℕ} (ha : 1 < a) (h : n < m) : a ^ n < a ^ m :=
+lt_of_lt_of_le
+  ((lt_mul_iff_one_lt_left (pow_pos (lt_trans zero_lt_one ha) _)).2 ha)
+  (pow_le_pow (le_of_lt ha) h)
+
+theorem pow_lt_pow' {a : α} {n m : ℕ} (ha : 1 < a) (h : n < m) : a ^ n < a ^ m :=
+begin
+  apply lt_of_lt_of_le,
+  { exact ((lt_mul_iff_one_lt_left (pow_pos (lt_trans zero_lt_one ha) _)).2 ha)},
+  exact (pow_le_pow (le_of_lt ha) h)
+end
+
+theorem pow_lt_pow'' {a : α} {n m : ℕ} (ha : 1 < a) (h : n < m) : a ^ n < a ^ m :=
+begin
+  apply lt_of_lt_of_le,
+  { refine (lt_mul_iff_one_lt_left _).2 ha,
+    refine pow_pos _ _,
+    -- got it
+    exact lt_trans zero_lt_one ha
+  },
+  { refine pow_le_pow _ h, -- dirty trick?
+    exact le_of_lt ha
+  }
+end
+
+-- I want a button which puts
+--set_option pp.all true
+-- on and off
+
 -- I've made them reals to keep with the theme of the sheet
 -- change the inequality if you think it goes the other way
-theorem Sht3Q3b : (100 : ℝ) ^10000 < 10000^100 := sorry 
+theorem Sht3Q3b : (10000 : ℝ) ^ 100 < (100 : ℝ) ^ 10000 := begin
+  rw (show 10000 = 100 * 100, by norm_num),
+  rw pow_mul,
+  -- next line doesn't work
+
+  -- apply pow_mono (by norm_num) (dec_trivial),
+  
+  -- but this variant does
+  refine pow_mono (by norm_num) (dec_trivial) _,
+  rw (show (10000 : ℝ) = 100 ^ 2, by norm_num),
+  exact pow_lt_pow (by norm_num) dec_trivial,
+end
 
 -- replace with the right explicit real number
 theorem Sht3Q03ci : nth_root (2 ^ 22) 2 = (0 : ℝ) := sorry
@@ -71,5 +137,4 @@ theorem Sht3Q03ci : nth_root (2 ^ 22) 2 = (0 : ℝ) := sorry
 theorem Sht3Q03cii : nth_root (2 ^ 2 ^ 22) 2 = (0 : ℝ) := sorry
 
 
--- found in original question
---theorem pow_lt_pow_of_lt {x i j : ℕ} : x > 1 → i < j → x^i < x^j :=
+
