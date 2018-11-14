1bf32c1c841d773342681de44becc1cd5cbd31a2
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-14 22:31:56 +0000

    finished question statement

--- src/example_sheet_03/Sht03Q05/Q0305.lean
+++ src/example_sheet_03/Sht03Q05/Q0305.lean
@@ -1,8 +1,12 @@
-import data.real.basic
+import analysis.real
 variables x t : ℝ
 
-theorem Q0305a (H:t>0) : abs x < t ↔ -t < x ∧ x < t := sorry
+noncomputable def abv (x : ℝ) := if x ≥ 0 then x else -x
 
-theorem Q0305b : abs(x+1)<3 ↔ -4<x ∧ x<2 := sorry
+theorem Q0305a (Ht : t > 0) : abv x < t ↔ -t < x ∧ x < t := sorry
 
-theorem Q0305c : abs (x-2) < abs (x-4) ↔ x < 3 := sorry
+-- change the RHS to the correct explicit range
+theorem Q0305b : abv (x + 1) < 3 ↔ x = 37 := sorry
+
+-- change the RHS to the correct explicit range
+theorem Q0305c : abv (x - 2) < abv (x - 4) ↔ x = 37 := sorry
