466bb79fca9ea5bfc68c6759aff31f8e0a3b7c37
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-14 18:31:53 +0000

    now compiles

--- src/example_sheet_02/Sht02Q01/Q0201.lean
+++ src/example_sheet_02/Sht02Q01/Q0201.lean
@@ -11,16 +11,20 @@ def countable_intersection_from_one {α : Type} (X : nat → set α) :=
 
 def Q0201a_sets (n : ℕ) : set ℝ := {x | ↑n ≤ x ∧ x < (n+1)}
 
-theorem Q0201a : countable_union_from_zero Q0201a_sets = ??? := sorry
+-- replace with the right set
+theorem Q0201a : countable_union_from_zero Q0201a_sets = {x : ℝ | x = 37} := sorry
 
 def Q0201b_sets (n : ℕ) : set ℝ := {x | 1/(↑n) ≤ x ∧ x ≤ 1}
 
-theorem Q0201b : countable_union_from_one Q0201b_sets = ??? := sorry 
+-- replace with the right set
+theorem Q0201b : countable_union_from_one Q0201b_sets = {x : ℝ | x = 37} := sorry 
 
 def Q0201c_sets (n : ℕ) : set ℝ := {x | -↑n < x ∧ x < n}
 
-theorem Q0201c : countable_union_from_one Q0201c_sets = ??? := sorry
+-- replace with the right set
+theorem Q0201c : countable_union_from_one Q0201c_sets = {x : ℝ | x = 37} := sorry
 
 def Q0201d_sets (n : ℕ) : set ℝ := {x | -↑n < x ∧ x < n}
 
-theorem Q0201d : countable_intersection_from_one Q0201d_sets = ??? := sorry
+-- replace with the right set
+theorem Q0201d : countable_intersection_from_one Q0201d_sets = {x : ℝ | x = 37} := sorry
