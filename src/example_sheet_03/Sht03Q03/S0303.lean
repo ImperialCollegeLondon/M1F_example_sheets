cf8de699ef2d74a5279da489e5d2a0da78ef315b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-14 13:05:01 +0000

    VS Code hover issue

--- src/example_sheet_03/Sht03Q03/S0303.lean
+++ src/example_sheet_03/Sht03Q03/S0303.lean
@@ -1,7 +1,10 @@
 import analysis.real
 import xenalib.real_nth_root
+import tactic.norm_num
 
 -- preliminary lemma
+
+open real 
 
 theorem pow_mono' {x : ℝ} {n : ℕ} (Hxpos : 0 < x)
   (Hnpos : 0 < n) (y : ℝ) : x < y → x ^ n < y ^ n :=
@@ -27,8 +30,14 @@ begin
   assumption
 end
 
+-- If I hover over the first nth_root_pos below, I don't get a transient
+-- hover box with the definition of the function. If I hover over
+-- the second, I do.
 theorem Sht3Q3a : nth_root 3 (3*10^12) < nth_root 2 (2 * 10 ^ 12) :=
 begin
-  
+  apply lt_of_pow_lt (nth_root_pos _ _) (nth_root_pos _ _),
+  repeat {sorry},
 end
 
+-- found in original question
+--theorem pow_lt_pow_of_lt {x i j : ℕ} : x > 1 → i < j → x^i < x^j :=
