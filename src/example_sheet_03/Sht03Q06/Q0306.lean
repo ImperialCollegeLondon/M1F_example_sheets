9a736390453bcedc96b00397ab0fefd6c80a4d21
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-15 01:19:50 +0000

    sheet 6 questions

--- src/example_sheet_03/Sht03Q06/Q0306.lean
+++ src/example_sheet_03/Sht03Q06/Q0306.lean
@@ -1,9 +1,40 @@
-#exit
--- mathlib broke my code
-import data.complex.basic
+-- Definition of the complexes from scratch, assuming
+-- everything about the real numbers, but proving everything
+-- about the complex numbers.
+
+import data.real.basic
+
+-- a complex number z is a pair of reals z.re and z.im
+structure complex : Type :=
+(re : ℝ) (im : ℝ)
+
+notation `ℂ` := complex
+
+namespace complex
+
+-- We define addition on complex numbers in the usual way
+protected definition add : ℂ → ℂ → ℂ :=
+λ z w, ⟨z.re + w.re, z.im + w.im⟩
+
+instance : has_add ℂ := ⟨complex.add⟩
+
+protected definition mul : ℂ → ℂ → ℂ :=
+λ z w, ⟨z.re * w.re- z.im * w.im, z.re * w.im + z.im * w.re⟩
+
+-- We set up the notation `*`
+instance : has_mul ℂ := ⟨complex.mul⟩
+
+definition conjugate : ℂ → ℂ :=
+λ z, ⟨z.re,-z.im⟩
+
+end complex
+
+open complex
+
 variables p q r : ℂ
-theorem Q6a : (p+q)+r=p+(q+r) := sorry
 
-theorem Q6b : p*q=q*p := sorry
+theorem Q6a : (p + q) + r = p + (q + r) := sorry
 
-theorem Q6c : complex.conjugate p * complex.conjugate q = complex.conjugate (p*q) := sorry
+theorem Q6b : p * q = q * p := sorry
+
+theorem Q6c : conjugate p * conjugate q = conjugate (p * q) := sorry
