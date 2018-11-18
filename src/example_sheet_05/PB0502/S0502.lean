4e293ea68bbdfdf68470397b1cdfb88c3ce18d00
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-18 13:58:30 +0000

    Not happy with this soln -- induction starting at 2 so messy!

--- src/example_sheet_05/PB0502/S0502.lean
+++ src/example_sheet_05/PB0502/S0502.lean
@@ -1,23 +1,28 @@
-import algebra.group_power tactic.norm_num algebra.big_operators
+import algebra.group_power
 
-
-theorem Q2 (n : ℕ) : n ≥ 2 → nat.pow 4 n > nat.pow 3 n + nat.pow 2 n :=
+theorem Q0502 (n : ℕ) : n ≥ 2 → 4 ^ n > 3 ^ n + 2 ^ n :=
 begin
-intro H_n_ge_2,
-cases n with n1,
-  exfalso,revert H_n_ge_2, exact dec_trivial,
-cases n1 with n2,
-  exfalso,revert H_n_ge_2, exact dec_trivial,
-clear H_n_ge_2,
-induction n2 with d Hd,
-  exact dec_trivial,
-let e := nat.succ (nat.succ d),
-show nat.pow 4 e*4>nat.pow 3 e*3+nat.pow 2 e*2,
-change nat.pow 4 (nat.succ (nat.succ d)) > nat.pow 3 (nat.succ (nat.succ d)) + nat.pow 2 (nat.succ (nat.succ d))
-with nat.pow 4 e>nat.pow 3 e+nat.pow 2 e at Hd,
-exact calc
-nat.pow 4 e * 4 > (nat.pow 3 e + nat.pow 2 e) * 4 : mul_lt_mul_of_pos_right Hd (dec_trivial)
-... = nat.pow 3 e*4+nat.pow 2 e*4 : add_mul _ _ _
-... ≥ nat.pow 3 e*3+nat.pow 2 e*4 : add_le_add_right (nat.mul_le_mul_left _ (dec_trivial)) _
-... ≥ nat.pow 3 e*3+nat.pow 2 e*2 : add_le_add_left (nat.mul_le_mul_left _ (dec_trivial)) _,
+  intro Hn2,
+  -- hypotheses    n : ℕ
+  --             Hn2 : n ≥ 2
+  -- now replace n with m + 2 and then do induction on m >= 0
+  cases n with n, cases Hn2, -- Hn2 : 0 ≥ 2 and cases kills it.
+  cases n with m, revert Hn2, exact dec_trivial, -- here Hn2 : 1 ≥ 2 and cases doesn't kill it
+  clear Hn2, -- and we're finally ready to go
+  induction m with e He,
+  -- it would have been nice just to be able to write modified_induction n Hn2 with d Hd...
+    exact dec_trivial, -- base case
+  generalize Hd : nat.succ (nat.succ e) = d,
+  rw Hd at He,
+  clear Hd e,
+  /- context is *finally*
+  e : ℕ,
+  Hd : 4 ^ e > 3 ^ e + 2 ^ e
+  ⊢ 4 ^ nat.succ e > 3 ^ nat.succ e + 2 ^ nat.succ e
+  -/
+  exact calc
+    4 ^ nat.succ d > (3 ^ d + 2 ^ d) * 4 : mul_lt_mul_of_pos_right Hd dec_trivial
+    ...            = 3 ^ d * 4 + 2 ^ d * 4 : by rw add_mul
+    ...            ≥ 3 ^ d * 3 + 2 ^ d * 4 : add_le_add_right (nat.mul_le_mul_left _ (dec_trivial)) _
+    ...            ≥ 3 ^ d * 3 + 2 ^ d * 2 : add_le_add_left (nat.mul_le_mul_left _ (dec_trivial)) _,
 end
