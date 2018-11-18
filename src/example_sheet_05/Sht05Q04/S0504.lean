6a0bf8093348041d7e5517158897c5f54b7a994d
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-18 17:52:38 +0000

    finished Q4

--- src/example_sheet_05/Sht05Q04/S0504.lean
+++ src/example_sheet_05/Sht05Q04/S0504.lean
@@ -1,52 +1,39 @@
-#exit
+import data.nat.basic
+import xenalib.modified_induction
+import tactic.norm_num
 
-import algebra.group_power tactic.norm_num algebra.big_operators
+open nat
 
-def factorial : ℕ → ℕ
-| 0 := 1
-| (n+1) := (factorial n) * (n+1)
-
-theorem Q4 (n : ℕ) (H : n > 0) : factorial n < 3^n ↔ n ≤ 6 :=
+definition meh (P Q : Prop) : (¬ Q → ¬ P) → (P → Q) := 
 begin
-cases n with m,
-  revert H,
-  exact dec_trivial,
-clear H,
-
-cases m with m,split;exact dec_trivial,
-cases m with m,split;exact dec_trivial,
-cases m with m,split;exact dec_trivial,
-
-cases m with m,split;intros,exact dec_trivial,
-repeat {unfold factorial monoid.pow},{norm_num},
-
-cases m with m,split;intros,exact dec_trivial,
-repeat {unfold factorial monoid.pow},{norm_num},
-
-cases m with m,split;exact dec_trivial,
-
-split,
-  tactic.swap,
-  intro H,
+  intros H1 H2,
+  cases classical.em Q,
+    assumption,
   exfalso,
-  revert H,
-  exact dec_trivial,
-
-intro this,exfalso,revert this,
-apply not_lt_of_ge,
-induction m with d Hd,
-repeat {unfold factorial monoid.pow},{norm_num},
-
-let e := nat.succ (nat.succ (nat.succ (nat.succ (nat.succ (nat.succ (nat.succ d)))))),
-change nat.succ (nat.succ (nat.succ (nat.succ (nat.succ (nat.succ (nat.succ d)))))) with e,
-change nat.succ (nat.succ (nat.succ (nat.succ (nat.succ (nat.succ (nat.succ d)))))) with e at Hd,
-
-have He_ge_3 : nat.succ e ≥ 3 := by exact dec_trivial,
-exact calc
-factorial (nat.succ e) = factorial e * (nat.succ e) : by unfold factorial
-... ≥ factorial e * 3 : nat.mul_le_mul_left _ He_ge_3
-... ≥ 3^e*3 : nat.mul_le_mul_right 3 Hd
-... = 3*3^e : by rw mul_comm
-... = 3^nat.succ e : rfl,
+  apply H1;assumption,
 end
 
+theorem Q4 (n : ℕ) (H : n > 0) : fact n < 3 ^ n ↔ n ≤ 6 :=
+begin
+  split, swap, -- let's do the easy case first
+    intro Hn6, revert H, revert Hn6, revert n, exact dec_trivial,
+  -- now the induction way
+  apply meh,
+  intro Htemp,
+  apply not_lt_of_ge,
+  replace Htemp : 7 ≤ n := lt_of_not_ge Htemp,
+  revert Htemp,
+  apply modified_induction, 
+    -- base case
+    show 7 * (6 * (5 * (4 * (3 * (2 * (1 * 1)))))) ≥ 3 ^ 7, -- just don't ask
+    norm_num,
+  -- inductive step
+  intros d Hd7 Hd,
+  refine calc 
+    fact (d + 1) = (d + 1) * fact d : rfl
+    ...          ≥ 3 * fact d       : nat.mul_le_mul_right _ _
+    ...          ≥ 3 * 3 ^ d        : nat.mul_le_mul_left _ Hd
+    ...          = 3 ^ d * 3        : by rw mul_comm,
+  apply succ_le_succ,
+  exact le_trans dec_trivial Hd7
+end
