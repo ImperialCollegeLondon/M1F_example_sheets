bbddd52de96d49215a9aefffcb78a5c7c023111a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-18 15:11:57 +0000

    Q3 done

--- src/example_sheet_05/Sht05Q03/Q0503.lean
+++ src/example_sheet_05/Sht05Q03/Q0503.lean
@@ -1,3 +1,6 @@
-theorem Q0503a (n : ℕ) : ∃ k, (n + 1) + (n + 2) +(n + 3) + (n + 4) = 4 * k + 2 := sorry
+import algebra.group_power
 
-theorem Q0503b (n : ℕ) : 8 ∣ 11^n - 3^n := sorry
+theorem Q0503a (n : ℕ) : ∃ k, (n + 1) + (n + 2) + (n + 3) + (n + 4) = 4 * k + 2 := sorry
+
+-- If you use nat then you have annoying problems with minus
+theorem Q0503b (n : ℕ) : 8 ∣ (11 : ℤ) ^ n - (3 : ℤ) ^ n := sorry
