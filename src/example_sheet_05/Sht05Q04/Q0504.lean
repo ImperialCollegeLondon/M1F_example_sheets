40bd1e6b52dda78ed857c94c3f65825c9e88ea6f
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-18 15:14:20 +0000

    question done

--- src/example_sheet_05/Sht05Q04/Q0504.lean
+++ src/example_sheet_05/Sht05Q04/Q0504.lean
@@ -1,7 +1,8 @@
-import algebra.group_power tactic.norm_num algebra.big_operators
+import data.nat.basic
 
-def factorial : ℕ → ℕ
-| 0 := 1
-| (n+1) := (factorial n) * (n+1)
+-- gives factorial (fact n)
 
-theorem Q4 (n : ℕ) (H : n > 0) : factorial n < 3^n ↔ n ≤ 6 := sorry
+open nat
+
+-- change RHS of iff to be the range you think it should be
+theorem Q4 (n : ℕ) (H : n > 0) : fact n < 3 ^ n ↔ n = 37 := sorry
