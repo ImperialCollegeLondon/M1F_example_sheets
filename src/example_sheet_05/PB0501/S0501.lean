056c19cafdae3eacadade891c6fe88da2e72ee40
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-18 13:26:43 +0000

    finished Q1

--- src/example_sheet_05/PB0501/S0501.lean
+++ src/example_sheet_05/PB0501/S0501.lean
@@ -1,4 +1,5 @@
 --import algebra.group_power tactic.norm_num algebra.big_operators
+import tactic.ring
 
 def Fib : ℕ → ℕ
 | 0 := 37 -- question only defines F_n for n>=1 so set F_0 = junk value
@@ -9,88 +10,70 @@ def Fib : ℕ → ℕ
 def is_even (n : ℕ) : Prop := ∃ k, n = 2 * k
 def is_odd (n : ℕ) : Prop := ∃ k, n = 2 * k + 1
 
--- starting at 0 would have been such a better idea
-theorem Q1a : ∀ n : ℕ, n ≥ 1 →
-  is_odd (Fib (3 * n - 2)) ∧ is_odd (Fib (3 * n - 1)) ∧ is_even (Fib (3 * n)) := sorry
-
-theorem Q1b : is_even (Fib (2018)) := sorry
-
---#eval Fib 10
---#reduce Fib 10
-
-def is_even (n : ℕ) : Prop := ∃ k, n=2*k
-def is_odd (n : ℕ) : Prop := ∃ k, n=2*k+1
-
 lemma even_of_even_add_even (a b : ℕ) : is_even a → is_even b → is_even (a+b) :=
 begin
-intros Ha Hb,
-cases Ha with k Hk,
-cases Hb with l Hl,
-existsi k+l,
-simp [Hk,Hl,add_mul],
+  intros Ha Hb,
+  cases Ha with k Hk,
+  cases Hb with l Hl,
+  existsi k+l,
+  simp [Hk,Hl,mul_add],
 end
 
 lemma odd_of_odd_add_even {a b : ℕ} : is_odd a → is_even b → is_odd (a+b) :=
 begin
-intros Ha Hb,
-cases Ha with k Hk,
-cases Hb with l Hl,
-existsi k+l,
-simp [Hk,Hl,add_mul],
+  intros Ha Hb,
+  cases Ha with k Hk,
+  cases Hb with l Hl,
+  existsi k+l,
+  simp [Hk,Hl,mul_add],
 end
 
 lemma odd_of_even_add_odd {a b : ℕ} : is_even a → is_odd b → is_odd (a+b) :=
 λ h1 h2, (add_comm b a) ▸ (odd_of_odd_add_even h2 h1)
 
-
 lemma even_of_odd_add_odd {a b : ℕ} : is_odd a → is_odd b → is_even (a+b) :=
 begin
-intros Ha Hb,
-cases Ha with k Hk,
-cases Hb with l Hl,
-existsi k+l+1,
--- simp [mul_add,Hk,Hl,one_add_one_eq_two] -- fails!
-rw [Hk,Hl,mul_add,mul_add],
-change 2 with 1+1,
-simp [mul_add,add_mul],
+  intros Ha Hb,
+  cases Ha with k Hk,
+  cases Hb with l Hl,
+  existsi k+l+1,
+  rw [Hk,Hl,mul_add,mul_add],
+  change 2 with 1+1,
+  simp [mul_add,add_mul],
 end
 
-theorem Q1a : ∀ n : ℕ, n ≥ 1 →
-  is_odd (Fib (3*n-2)) ∧ is_odd (Fib (3*n-1)) ∧ is_even (Fib (3*n)) :=
+-- starting at 0 would have been such a better idea
+theorem Q0501a : ∀ n : ℕ, n ≥ 1 →
+  is_odd (Fib (3 * n - 2)) ∧ is_odd (Fib (3 * n - 1)) ∧ is_even (Fib (3 * n)) :=
 begin
-intros n Hn,
-cases n with m,
-have : ¬ (0 ≥ 1) := dec_trivial,
-exfalso,
-exact (this Hn),
-induction m with d Hd,
-  exact ⟨⟨0,rfl⟩,⟨0,rfl⟩,⟨1,rfl⟩⟩,
-change 3*nat.succ d-2 with 3*d+1 at Hd,
-change 3*nat.succ d -1 with 3*d+2 at Hd,
-change 3*nat.succ d with 3*d+3 at Hd,
-change 3*nat.succ (nat.succ d)-2 with 3*d+4,
-change 3*nat.succ (nat.succ d)-1 with 3*d+5,
-change 3*nat.succ (nat.succ d) with 3*d+6,
-
-let Hyp := Hd (begin apply nat.succ_le_succ,exact nat.zero_le d,end),
-let H1 := Hyp.left,
-let H2 := Hyp.right.left,
-let H3 := Hyp.right.right,
-have H4 : is_odd (Fib (3*d+4)),
-  change Fib (3*d+4) with Fib (3*d+2)+Fib(3*d+3),
-  exact odd_of_odd_add_even H2 H3,
-have H5 : is_odd (Fib (3*d+5)),
-  change Fib (3*d+5) with Fib (3*d+3)+Fib(3*d+4),
-  exact odd_of_even_add_odd H3 H4,
-have H6 : is_even (Fib (3*d+6)),
-  change Fib (3*d+6) with Fib (3*d+4)+Fib(3*d+5),
-  exact even_of_odd_add_odd H4 H5,
-exact ⟨H4,H5,H6⟩,
+  intros n Hn,
+  cases n with m, cases Hn, -- stupid n = 0 case
+  clear Hn,
+  induction m with d Hd,
+    -- n = 1 case
+    exact ⟨⟨0,rfl⟩,⟨0,rfl⟩,⟨1,rfl⟩⟩,
+  -- general case
+  rcases Hd with ⟨H3dp1,H3dp2,H3dp3⟩,
+  -- one could tidy up now, with
+  --rw (show 3 * (nat.succ d) - 2 = 3 * d + 1, by ring) at H3dp1,
+  --rw (show 3 * (nat.succ d) - 1 = 3 * d + 2, by ring) at H3dp2,
+  --rw (show 3 * (nat.succ d) = 3 * d + 3, by ring) at H3dp3,
+  --but this is a false economy, all these things are true by refl
+  -- and Lean can just work them out by itself. 
+  -- We can just go straight to the proof.
+  have H3dp4 : is_odd (Fib (3 * d + 4)),
+    rw [Fib], exact odd_of_odd_add_even H3dp2 H3dp3,
+  have H3dp5 : is_odd (Fib (3 * d + 5)),
+    rw [Fib], exact odd_of_even_add_odd H3dp3 H3dp4,
+  have H3dp6 : is_even (Fib (3 * d + 6)),
+    rw [Fib], exact even_of_odd_add_odd H3dp4 H3dp5,
+  rw (show 3 * (nat.succ (nat.succ d)) - 2 = 3 * d + 4, by ring),
+  exact (⟨H3dp4,H3dp5,H3dp6⟩ : _ ∧ _ ∧ _)
 end
 
-theorem Q1b : is_odd (Fib (2017)) :=
+theorem Q0501b : is_odd (Fib (2018)) :=
 begin
-have H : 2017 = 3*673-2 := dec_trivial,
-rw [H],
-exact (Q1a 673 (dec_trivial)).left,
+  have H : 2018 = 3 * 673 - 1 := dec_trivial,
+  rw H,
+  exact (Q0501a _ dec_trivial).right.left
 end
