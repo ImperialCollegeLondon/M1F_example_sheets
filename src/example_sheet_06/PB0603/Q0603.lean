79d87194b0440df8b541eb3ba58b63e1d593cb2f
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2018-11-19 11:59:13 +0000

    question formalised

--- src/example_sheet_06/PB0603/Q0603.lean
+++ src/example_sheet_06/PB0603/Q0603.lean
@@ -1,3 +1,8 @@
-import analysis.real tactic.norm_num algebra.group_power
+import data.real.basic
 
+definition is_bounded_above (S : set ℝ) := ∃ b : ℝ, ∀ s ∈ S, s ≤ b
 
+-- chose which one to prove, comment out the other.
+
+theorem Q0603_yes : ∃ S : set ℝ, (∀ s ∈ S, ∃ t ∈ S, s < t) ∧ is_bounded_above S := sorry 
+theorem Q0603_no : ∀ S : set ℝ, (∀ s ∈ S, ∃ t ∈ S, s < t) → ¬ is_bounded_above S := sorry
